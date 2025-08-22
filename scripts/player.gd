5f00d707421ef7024081bdb16e24d06432a1ab70
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-08-23 01:33:24 +0800

    added initial code for collecting trash

--- scripts/player.gd
+++ scripts/player.gd
@@ -5,6 +5,7 @@ class_name Player
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
+
 func _physics_process(delta: float) -> void:      
 	player_gravity(delta)
 	player_jump()
@@ -17,3 +18,19 @@ func player_jump() -> void:
 func player_gravity(delta: float) -> void:
 	if not is_on_floor():
 		velocity += get_gravity() * delta
+
+
+func _on_trash_collection_area_body_entered(body: Node2D) -> void:
+	if body is Trash:
+		var trash_type = body.type
+		print_debug("Collected Trash: " + trash_type)
+		increment_trash(trash_type)
+
+func increment_trash(type: Utils.TrashType):
+	if type == Utils.TrashType.Recyclable:
+		Game.collected_recyclable += 1
+	if type == Utils.TrashType.Biodegradable:
+		Game.collected_biodegradable += 1
+	if type == Utils.TrashType.ToxicWaste:
+		Game.collected_toxic_waste += 1
+	
