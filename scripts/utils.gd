08bc7fc2421a4b592a4fba4a818b402528023a6c
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-08-23 15:51:37 +0800

    added a utility function and added an energy bar script

--- scripts/utils.gd
+++ scripts/utils.gd
@@ -5,3 +5,9 @@ enum TrashType {
 	Biodegradable,
 	ToxicWaste
 }
+
+func get_enum_name(enum_dict: Dictionary, value: int) -> String:
+	for key in enum_dict.keys():
+		if enum_dict[key] == value:
+			return key
+	return ""
