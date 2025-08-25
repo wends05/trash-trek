e92829bdf84a0c2dec55aa349f73a6a078e65743
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-08-25 22:52:31 +0800

    ,

--- scripts/trash_remover.gd
+++ scripts/trash_remover.gd
@@ -6,3 +6,6 @@ func _on_area_entered(area: Area2D) -> void:
 	if area.get_parent() is Trash:
 		var trash : Trash = area.get_parent()
 		trash.remove()
+	if area.get_parent() is TrashBin:
+		var trash_bin : TrashBin = area.get_parent()
+		trash_bin.remove()
