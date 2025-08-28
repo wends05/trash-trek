8169f458fe92c6080c582082f01e9b179fc601a3
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-08-29 03:05:58 +0800

    implemented initial throw trash logic

--- scripts/trash_bin.gd
+++ scripts/trash_bin.gd
@@ -5,10 +5,12 @@ class_name TrashBin
 
 @export var type: Utils.TrashType
 @export var terrain_manager: TerrainManager
+@onready var player_collision_area: StaticBody2D = $PlayerCollisionArea
 
 func _ready() -> void:
 	var texture_path = "res://assets/trashbins/%s.png" % Utils.get_enum_name(Utils.TrashType, type)
 	$Asset.texture = load(texture_path)
+	Game.changed_trash_type.connect(change_player_collision)
 
 func _physics_process(delta: float) -> void:
 	position.x -= terrain_manager.speed * delta
@@ -16,3 +18,7 @@ func _physics_process(delta: float) -> void:
 func _on_area_area_entered(area: Area2D) -> void:
 	if area is GroundArea:
 		area.bring_trash_above(self, 25)
+
+func change_player_collision(trash_type: Utils.TrashType) -> void:
+	print("CHANGING COLLISION")
+	player_collision_area.set_collision_layer_value(4, type != trash_type)
