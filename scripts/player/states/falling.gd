d1d94e1fe21f18a2c0f7720798976c93332503b5
Padre <nonoyjpgerona@gmail.com>
2025-09-01 23:39:55 +0800

    Add player scene and refine falling state script formatting

--- scripts/player/states/falling.gd
+++ scripts/player/states/falling.gd
@@ -4,17 +4,17 @@ extends State
 @onready var player: Player = get_parent().get_parent()
 
 func enter():
-  animated_sprite.play("falling")
+	animated_sprite.play("falling")
 
 func physics_update(_delta: float):
   # Apply gravity
-  player.velocity.y += player.get_gravity().y * _delta
-  player.move_and_slide()
+	player.velocity.y += player.get_gravity().y * _delta
+	player.move_and_slide()
 
   # Transition to running if landed
-  if player.is_on_floor():
-    transitioned.emit(self, "running")
-    return
+	if player.is_on_floor():
+		transitioned.emit(self, "running")
+		return
 
 func exit():
-  pass
+	pass
