edcff6dab14aa0da5a29da8d75ce208914a0dbba
Wendell Terence Dador <wendellterence.dador-23@cpu.edu.ph>
2025-09-03 02:49:45 +0800

    fixed ui script

--- scripts/TimeLabel..gd
+++
@@ -1,9 +0,0 @@
-extends Label
-
-func _ready() -> void:
-	Game.time_changed.connect(update_time)
-
-func update_time(time: float) -> void:
-	var minutes = int(time) / 60
-	var seconds = int(time) % 60
-	text = "%02d:%02d" % [minutes, seconds]
