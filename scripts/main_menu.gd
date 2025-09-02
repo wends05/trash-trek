34157115007f7705eb5a5a98eb323cdcd17f5dd1
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-09-03 01:22:59 +0800

    Update UI

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,5 +1,12 @@
 extends Control
 
+
+func _on_start_button_pressed() -> void:
+	start_game()
+
+func _on_exit_button_pressed() -> void:
+	quit_game()
+
 func start_game():
 	Game.reset_stats()
 	get_tree().change_scene_to_file("res://scenes/Main.tscn")
