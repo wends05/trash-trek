36ee76150dfb4ba2710404dda77c2da9a067f0ef
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-09-03 02:52:32 +0800

    Update and  Add Scene tRansition

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,15 +1,28 @@
 extends Control
 
+@onready var GameTitle = $CanvasLayer2/GameTitle
 
+func _ready():
+	is_intro_scene()
+	
 func _on_start_button_pressed() -> void:
 	start_game()
 
 func _on_exit_button_pressed() -> void:
 	quit_game()
 
+func is_intro_scene():
+	if SceneHandler.last_scene_path == "res://scenes/intro.tscn":
+		GameTitle.play("text_pop")
+		await GameTitle.animation_finished
+		GameTitle.play("flicker")
+		await GameTitle.animation_finished
+		GameTitle.play("default")
+		
 func start_game():
+	SceneHandler.last_scene_path = get_tree().current_scene.scene_file_path
 	Game.reset_stats()
-	get_tree().change_scene_to_file("res://scenes/Main.tscn")
+	SceneTransition.change_scene("res://scenes/Main.tscn")
 
 func quit_game():
 	get_tree().quit()
