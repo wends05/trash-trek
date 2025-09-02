ca4cf4653373a167669d88c3334c15e8f135ca1b
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-09-03 04:44:04 +0800

    Revert "Revert changes"
    This reverts commit 33007bb00cb2c6bd28e90d0f6a5d59bac2e3a5fc.

--- scripts/trash_bin.gd
+++ scripts/trash_bin.gd
@@ -4,30 +4,67 @@ class_name TrashBin
 
 
 var type: Utils.TrashType
-@onready var player_collision_area: StaticBody2D = $PlayerCollisionArea
-@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
 var trash_bin_resource: TrashBinResource
 
+@onready var player_collision_area: StaticBody2D = $PlayerCollisionArea
+@onready var animated_sprite: AnimatedSprite2D = $AnimatedSprite2D
+@onready var icon: TextureRect = $Icon
+
+@onready var debug_label: Label = $"debug label"
+
+var animating = false
+var collected = false
+
+var amount_required: int = 0
+@onready var amount_required_label: Label = $AmountRequiredLabel
+
 func _ready() -> void:
-	Game.changed_trash_type.connect(change_player_collision)
 	load_trash_bin()
-	
+	Game.changed_trash_type.connect(change_player_collision)
+	Game.trash_collected.connect(change_player_collision)
+	amount_required = [3, 6, 9].pick_random()
+	amount_required_label.text = "%s" % amount_required
+
+	update_debug_label()
 
 func load_trash_bin():
-	trash_bin_resource = Utils.get_random_trash_bin()
+	trash_bin_resource = Utils.get_trash_bin(type)
 
 	animated_sprite.sprite_frames = trash_bin_resource.sprite_frames
 	type = trash_bin_resource.type
+	icon.texture = trash_bin_resource.icon_image
+
 	change_player_collision(Game.selected_trash_type)
-	
 	animated_sprite.play("idle")
 
 
 func change_player_collision(trash_type: Utils.TrashType) -> void:
-	print("CHANGING COLLISION")
-	player_collision_area.set_collision_layer_value(4, type != trash_type)
+	var has_enough: bool = Game.get_trash_count(trash_type) >= amount_required
+	var should_block: bool = not (has_enough and trash_type == type)
+	player_collision_area.set_collision_layer_value(4, should_block)
+	update_debug_label()
+
+
+func update_debug_label() -> void:
+	debug_label.text = \
+		"Collision: %s\nTrash Count: %s\n" % [
+			player_collision_area.get_collision_layer_value(4),
+			Game.get_trash_count(type)
+		]
 
 func throw_trash() -> void:
+	if animating:
+		return
+	if Game.get_trash_count(type) < amount_required:
+		return
+	if collected:
+		return
+	collected = true
+	Game.decrease_trash_count(type, amount_required)
+	Game.add_energy(amount_required)
+	animating = true
 	animated_sprite.play("open")
 	await animated_sprite.animation_finished
 	animated_sprite.play("idle")
+	animating = false
+	change_player_collision(Game.selected_trash_type)
