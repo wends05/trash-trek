a4e62b4dccb8bc56faa800b74b6512163e4d85c0
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-09-07 15:53:29 +0800

    Update Menu Ui

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -9,7 +9,7 @@ extends Control
 
 @onready var start_button = $Buttons/StartButton
 @onready var tutorial_button = $Buttons/TutorialButton
-@onready var credit_button = $Buttons/CreditButton
+@onready var credits_button = $Buttons/CreditsButton
 @onready var exit_button = $Buttons/ExitButton
 
 func _ready():
@@ -28,6 +28,10 @@ func _on_tutorial_button_pressed() -> void:
 	Utils.anim_player(buttons_player, "tutorial_press")
 	await buttons_player.animation_finished
 
+func _on_credits_button_pressed() -> void:
+	Utils.anim_player(buttons_player, "credits_press")
+	await buttons_player.animation_finished
+
 func is_intro_scene():
 	if SceneHandler.last_scene_path == "res://scenes/intro.tscn":
 		Utils.anim_player(props_player, "cam_in")
@@ -42,10 +46,10 @@ func is_intro_scene():
 
 func start_game():
 	player.hide()
-	disable_buttons([credit_button, exit_button, start_button, tutorial_button])
+	disable_buttons([credits_button, exit_button, start_button, tutorial_button])
 	Game.reset_stats()
 	SceneHandler.last_scene_path = get_tree().current_scene.scene_file_path
-	Utils.anim_playerr(props_player, "fade_out")
+	Utils.anim_player(props_player, "fade_out")
 	SceneTransition.change_scene("res://scenes/Main.tscn")
 
 func quit_game():
@@ -66,3 +70,9 @@ func _on_exit_button_mouse_entered() -> void:
 
 func _on_exit_button_mouse_exited() -> void:
 	$Buttons/ExitButton/ExitLabel.add_theme_color_override("font_color", Color.html("da6800"))
+
+func _on_credits_button_mouse_entered() -> void:
+	$Buttons/CreditsButton/CreditsLabel.add_theme_color_override("font_color", Color.html("f07a00"))
+
+func _on_credits_button_mouse_exited() -> void:
+	$Buttons/CreditsButton/CreditsLabel.add_theme_color_override("font_color", Color.html("da6800"))
