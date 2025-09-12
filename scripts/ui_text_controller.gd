ae503b4ebf90be48b9df3d5cd50e7b3d75c6cd76
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-09-13 05:32:54 +0800

    Update ui and ui_text_controller

--- scripts/ui_text_controller.gd
+++ scripts/ui_text_controller.gd
@@ -7,8 +7,11 @@ class_name UITextController
 var displayed_coins := 0
 var displayed_score := 0
 var animating = false
-var score_collected := Game.calculate_score()
-var coins_collected :=  Game.calculate_coins()
+var animating_coins := false
+var animating_score := false
+var score_collected
+var coins_collected
+
 func update_ui_text(state: Utils.UIStateType, reason: Utils.GameOverReason) -> void:
 	match state:
 		Utils.UIStateType.PauseMenu:
@@ -23,12 +26,24 @@ func update_ui_text(state: Utils.UIStateType, reason: Utils.GameOverReason) -> v
 				Utils.GameOverReason.OutOfEnergy:
 					game_reason_label.text = "Player ran out of energy"
 			
-			var energy = Game.energy
-			#var coins_collected := Game.calculate_coins()
+			#var energy = Game.energy
+			coins_collected = Game.calculate_coins()
+			score_collected = Game.calculate_score()
+			
 			var score := Game.calculate_score()
 			var distance := Game.distance_traveled
 			var trash := Game.accumulated_trash
-			animating = true
+			
+			displayed_coins = 0
+			displayed_score = 0
+			await	$"..".text_player.animation_finished
+			var timer = get_tree().create_timer(0.3)
+			timer.timeout.connect(func():
+				animating = true
+				animating_coins = true
+				animating_score = false
+			)
+		
 			#var text = ""
 			#if distance >= 1000:
 				#distance = round(distance / 1000.0 * 100) / 100.0
@@ -38,24 +53,49 @@ func update_ui_text(state: Utils.UIStateType, reason: Utils.GameOverReason) -> v
 				#text += "Distance Traveled: %d m\n" % distance
 				#
 			#text += "Trash Collected: %d\n " % trash 
-		#
+		
 			#if energy > 0:
 				#text += "Energy left With: %d\n" % energy
 			#
-			#text += "badges Gained:     %d\n Overall Score: %.f\n" % [coins_collected, score]
+			#text += "badges Gained:    %d\n Overall Score: %.f\n" % [coins_collected, score]
 			#text += ""
 			#game_stats_label.text = text
+			
+			
 
 func _process(delta: float) -> void:
-		if animating:
-			await $"..".trans_player.animation_finished	
-			while displayed_coins < coins_collected:
+	if animating:
+		var updated := false
+
+		# Step 1: Animate coins first
+		if animating_coins:
+			if displayed_coins < coins_collected:
 				displayed_coins += 1
-				print(displayed_coins)
-				update_game_stats()
-				await get_tree().create_timer(0.05).timeout
-				
+				updated = true
+			else:
+				animating_coins = false
+				await_delay_then_start_score()
+
+		# Step 2: Animate score after coins
+		elif animating_score:
+			if displayed_score < score_collected:
+				displayed_score += 1
+				updated = true
+			else:
+				animating_score = false
+				animating = false
+
+		if updated:
+			update_game_stats()
+
+
 func update_game_stats() -> void:
-	print("testt")
-	var text = "Badges Gained: %d\nOverall Score: %d\n" % [displayed_coins, displayed_score]
-	game_stats_label.text = text
+	var text = ""
+	if animating_coins:
+		game_stats_label.text = "Badges Gained: %d" % displayed_coins
+	elif animating_score:
+		game_stats_label.text = "Overall Score: %d" % displayed_score
+
+func await_delay_then_start_score() -> void:
+	await get_tree().create_timer(0.5).timeout 
+	animating_score = true
