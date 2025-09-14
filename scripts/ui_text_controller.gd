791587e17da5db445c97256cd8076027247ed5f9
Trasadas, Dainz Andrei <trasadasdainz@gmail.com>
2025-09-15 04:23:46 +0800

    Update UI

--- scripts/ui_text_controller.gd
+++ scripts/ui_text_controller.gd
@@ -127,20 +127,20 @@ func _process(delta: float) -> void:
 func update_game_stats() -> void:
 	
 	var text = "[center]"
-	text += "Overall Score: \n[color=white][font_size=30]%d[/font_size][/color]\n" % displayed_score
+	text += "[font_size=30]Overall Score:[/font_size] \n[color=white][font_size=50]%d[/font_size][/color]\n" % displayed_score
 
 	if trash_collected <= 0:
-		text += "[img=40x50]res://assets/buttons/button1.png[/img] [font_size=20]0[/font_size]  \n"
+		text += "[img=40x50]res://assets/buttons/button1.png[/img] [font_size=20]0[/font_size]  "
 	else:
-		text += "[img=40x50]res://assets/buttons/button1.png[/img] [font_size=20]%d[/font_size]  \n" % displayed_trash
+		text += "[img=40x50]res://assets/buttons/button1.png[/img] [font_size=20]%d[/font_size]  " % displayed_trash
 
 	if displayed_distance >= 1000:
 		var km = float(displayed_distance) / 1000.0
-		text += "  [img=40x50]res://assets/menu/player_run.png[/img] [font_size=20]%.2f[/font_size] km\n" % km
+		text += "  [img=40x50]res://assets/menu/player_run.png[/img] [font_size=20]%.2f[/font_size] km " % km
 	else:
-		text += "  [img=40x50]res://assets/menu/player_run.png[/img] [font_size=20]%d m[/font_size]\n" % displayed_distance
+		text += "  [img=40x50]res://assets/menu/player_run.png[/img] [font_size=20]%d m[/font_size] " % displayed_distance
 
-	text += "[img=50x30]res://assets/menu/badge.png[/img] [font_size=20]%d[/font_size]  \n" % displayed_coins
+	text += "[img=50x30]res://assets/menu/badge.png[/img][font_size=20]%d[/font_size] " % displayed_coins
 	#if animating_score:
 		#text += "Overall Score: %d\n" % displayed_score
 	#if animating_trash or animating_distance or animating_coins or animating_score:
